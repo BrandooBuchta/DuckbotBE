d0e5ae554d7fc9d38c63aa852b2b7f0f19373883
BrandooBuchta <buchta@brandoo.cz>
2025-01-04 18:58:37 +0100

    send

--- crud/user.py
+++ crud/user.py
@@ -48,9 +48,9 @@ def get_audience(db: Session, bot_id: UUID, for_client: bool, for_new_client: bo
     if for_client and for_new_client:
         return db.query(User).filter(User.bot_id == bot_id).all()
     if for_client and not for_new_client:
-        return db.query(User).filter(User.bot_id == bot_id, User.is_in_betfin == False).all()
-    if not for_client and for_new_client:
         return db.query(User).filter(User.bot_id == bot_id, User.is_in_betfin == True).all()
+    if not for_client and for_new_client:
+        return db.query(User).filter(User.bot_id == bot_id, User.is_in_betfin == False).all()
     if not for_client and not for_new_client:
         return []
 
