58d3284d715c303c7d21f54ebbc1ff2905e2d55c
BrandooBuchta <buchta@brandoo.cz>
2025-01-30 22:22:25 +0100

    test

--- schemas/user.py
+++ schemas/user.py
@@ -4,6 +4,14 @@ from pydantic import BaseModel
 from typing import Optional
 from uuid import UUID
 
+
+class UserCreate(UserBase):
+    bot_id: UUID
+    chat_id: int
+    from_id: int
+    is_client: bool = False
+    name: Optional[str] = None
+
 class UserBase(BaseModel):
     id: UUID
     bot_id: UUID
@@ -14,10 +22,3 @@ class UserBase(BaseModel):
 
     class Config:
         form_attributes = True
-
-class UserCreate(UserBase):
-    bot_id: UUID
-    chat_id: int
-    from_id: int
-    is_client: bool = False
-    name: Optional[str] = None
