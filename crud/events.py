1fde26c091380a5de4e1d7a616a0ad5711335e52
BrandooBuchta <buchta@brandoo.cz>
2025-03-09 16:28:54 +0100

    .

--- crud/events.py
+++ crud/events.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from typing import Optional
 import os
+import requests
 
 def get_event_date(event_name: str) -> Optional[datetime]:
     SUPABASE_ANON_KEY = os.getenv("SUPABASE_ANON_KEY")
