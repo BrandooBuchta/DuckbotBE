6135165715734f1256c86ae017162d839519f260
BrandooBuchta <buchta@brandoo.cz>
2025-04-04 19:00:22 +0200

    test

--- utils/messages.py
+++ utils/messages.py
@@ -5,19 +5,17 @@ from uuid import UUID
 def get_messages(level: int, lang: str, is_event: bool, bot_id: UUID):
     event_path = "event/" if is_event else "online/"
 
-    CUSTOM_PATH = f"data/customs/{bot_id}/level-{level}.json"
+    custom_path = f"data/customs/{bot_id}/level-{level}.json"
+    sequences_path = f"data/traces/{lang}/{event_path}level-{level}.json"
 
-    if not os.path.exists(CUSTOM_PATH):
-        raise FileNotFoundError(f"Soubor {CUSTOM_PATH} neexistuje")
+    if os.path.exists(custom_path):
+        path = custom_path
+    elif os.path.exists(sequences_path):
+        path = sequences_path
+    else:
+        raise FileNotFoundError(f"Nenalezen ani {custom_path}, ani {sequences_path}")
 
-    SEQUENCES_FILE_PATH = f"data/traces/{lang}/{event_path}level-{level}.json"
-
-    if not os.path.exists(SEQUENCES_FILE_PATH):
-        raise FileNotFoundError(f"Soubor {SEQUENCES_FILE_PATH} neexistuje")
-
-    PATH = CUSTOM_PATH if CUSTOM_PATH else SEQUENCES_FILE_PATH
-
-    with open(PATH, "r", encoding="utf-8") as file:
+    with open(path, "r", encoding="utf-8") as file:
         data = json.load(file)
 
     return data.get("messages", [])
