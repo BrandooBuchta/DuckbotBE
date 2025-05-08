8f3b176edadb284d738d1d2333478ae48c772bfd
BrandooBuchta <buchta@brandoo.cz>
2025-05-09 00:39:12 +0200

    any type was missing

--- routers/bot.py
+++ routers/bot.py
@@ -17,7 +17,7 @@ from base64 import b64encode, b64decode
 import os
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
-from typing import List, Dict, Literal
+from typing import List, Dict, Literal, Any
 import requests
 from uuid import UUID
 import random
